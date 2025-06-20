fb2e0ed6c2615eb34e71fe7dda3283799d123bb6
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 07:37:09 +0000

    [automatic commit] 2025-06-20 07:37:09 UTC 1.158.188.108

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -63,7 +63,11 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-  
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500)
+    }
+  })
 })
 
 module.exports = router;

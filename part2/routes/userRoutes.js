9be32c367b8183719b4509a7c6bf68007b685700
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 06:39:17 +0000

    [automatic commit] 2025-06-20 06:39:17 UTC 1.158.188.108

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -12,6 +12,8 @@ router.get('/', async (req, res) => {
   }
 });
 
+roouter.get('/')
+
 // POST a new user (simple signup)
 router.post('/register', async (req, res) => {
   const { username, email, password, role } = req.body;

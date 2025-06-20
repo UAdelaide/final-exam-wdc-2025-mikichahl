6cc4faeedf1e4791206c0ac08ef16e36bc082a5b
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 08:02:26 +0000

    [automatic commit] 2025-06-20 08:02:26 UTC 1.158.188.108

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -26,8 +26,10 @@ router.get('/dogs/mine', async (req, res) => {
   try {
     const [rows] = await db.query (`
     SELECT dog_id, name FROM Dogs WHERE owner_id = ?
-    `, [req.session.user.user_id])
-  }
+    `, [req.session.user.user_id]
+    );
+    res.json(rows);
+  } catch (err0)
 })
 
 // POST a new walk request (from owner)

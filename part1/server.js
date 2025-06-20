13e0421c544ef9e60e5d0d0ccee8e8a1610735be
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 03:33:51 +0000

    [automatic commit] 2025-06-20 03:33:50 UTC 1.158.188.108

--- part1/server.js
+++ part1/server.js
@@ -13,16 +13,3 @@ async function connectDB() {
     console.log('Connected to DB');
 }
 
-async function seedData() {
-    try {
-        const [users] = await db.query("SELECT COUNT(*) AS count FROM Users");
-        if (users[0].count === 0) {
-            await.db.query(`
-            INSERT INTO Users (username, email, password_hash, role) VALUES
-            ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-            ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-            ('carol123', 'carol@example.com', 'hashed321', 'owner'),
-            `)
-        }
-    }
-}

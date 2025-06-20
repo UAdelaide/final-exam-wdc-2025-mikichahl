68cb970a800597076593c2903bd46881f6d63c8f
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 03:46:54 +0000

    [automatic commit] 2025-06-20 03:46:53 UTC 1.158.188.108

--- part1/server.js
+++ part1/server.js
@@ -54,6 +54,11 @@ async function connectDB() {
                 u.username AS walker_username,
                 COUNT(r.rating_id) AS total_ratings,
                 ROUND(AVG(r.rating), 1) AS average_rating,
+                (
+                    SELECT COUNT(*)
+                    FROM WalkRequests wr
+                    
+                )
             `)
         }
     })

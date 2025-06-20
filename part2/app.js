e512c1abcb697e5023979bc2e7f5ae61e9e548b6
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 11:07:13 +0000

    [automatic commit] 2025-06-20 11:07:13 UTC 1.158.188.108

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const db = require('../models/db');
+
 require('dotenv').config();
 
 

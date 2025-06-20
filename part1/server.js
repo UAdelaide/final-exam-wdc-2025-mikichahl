a5d734d3944abc9bcafb19d7c4ea85b98a478289
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 03:34:56 +0000

    [automatic commit] 2025-06-20 03:34:55 UTC 1.158.188.108

--- part1/server.js
+++ part1/server.js
@@ -15,7 +15,7 @@ async function connectDB() {
     app.get('/api/dogs', async (req, res) => {
         try {
             const [rows] = await db.query(`
-            
+                SELECT d.name AS dog_name, d.size, u.username AS owner_username
             `)
         }
     })

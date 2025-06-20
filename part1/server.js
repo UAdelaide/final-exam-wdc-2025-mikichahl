d9ce745a0994fcb676489a8f4a2568d074d4b4dd
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 03:32:23 +0000

    [automatic commit] 2025-06-20 03:32:23 UTC 1.158.188.108

--- part1/server.js
+++ part1/server.js
@@ -20,8 +20,8 @@ async function seedData() {
             await.db.query(`
             INSERT INTO Users (username, email, password_hash, role) VALUES
             ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-('carol123', 'carol@example.com', 'hashed321', 'owner'),
+            ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+            ('carol123', 'carol@example.com', 'hashed321', 'owner'),
             `)
         }
     }

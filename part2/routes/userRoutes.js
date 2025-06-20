3fbbb76e0989c9eea065db1d0a54fb498a21a719
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 07:10:56 +0000

    [automatic commit] 2025-06-20 07:10:56 UTC 1.158.188.108

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -12,15 +12,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/', async (req, res) => {
-  try {
-    const [rows] = await db.query('SELECT user_id, username, email, role FROM Users');
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch users' });
-  }
-});
-
 // POST a new user (simple signup)
 router.post('/register', async (req, res) => {
   const { username, email, password, role } = req.body;

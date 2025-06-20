e510dbf9eba7adcb55a0781a23c917d6dcbad491
Miki Chahl <a1850759@adelaide.edu.au>
2025-06-20 07:30:57 +0000

    [automatic commit] 2025-06-20 07:30:57 UTC 1.158.188.108

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,6 +49,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    // save session for later
     req.session.user = {
       user_id: rows[0].user_id,
       username: rows[0].username,
